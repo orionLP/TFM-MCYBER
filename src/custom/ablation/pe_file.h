f7619e192eeb771a0eca4005131e65062b2642c6
oriol.lopez.petit@estudiantat.upc.edu <oriol.lopez.petit@estudiantat.upc.edu>
2026-05-22 16:17:16 +0200

    Add new interfaces for sections

--- src/custom/ablation/pe_file.h
+++ src/custom/ablation/pe_file.h
@@ -1,6 +1,8 @@
 #ifndef PE_FILE
 #define PE_FILE
 
+#include <stdint.h>
+
 #define PE_READ_MODE "rb"
 #define PE_WRITE_MODE "wb"
 #define PE_READWRITE_MODE "rb+"
@@ -9,6 +11,12 @@ typedef struct pe_file pe_file;
 
 pe_file *read_pe_file(char *path, char *mode);
 void close_pe_file(pe_file *file);
+
+int number_of_sections(pe_file *file);
+const char *section_name(int number);
+int section_number(const char *name);
+int section_size(const char *name);
+int write_to_section(const char *name, uint8_t value, int amount, int offset);
 
 void print_headers(pe_file *file);
 
