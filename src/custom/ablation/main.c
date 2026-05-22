ce413afb19478333c32c42fa21c9ad0269fed0a8
oriol.lopez.petit@estudiantat.upc.edu <oriol.lopez.petit@estudiantat.upc.edu>
2026-05-22 22:47:02 +0200

    Create function to do permutations

--- src/custom/ablation/main.c
+++ src/custom/ablation/main.c
@@ -5,6 +5,22 @@
 #include <math.h>
 #include <stdbool.h>
 
+void do_combination(bool *combination, const int n, int k, int remaining){
+    if(remaining == 0){
+        //act here
+
+        for(int i = 0; i < n; i++){
+            printf("%d ", combination[i]);
+        }
+        printf("\n");
+    } else{
+        for(int i = k; i < (n - remaining + 1); i++){
+            combination[i] = true;
+            do_combination(combination, n, i + 1, remaining - 1);
+            combination[i] = false;
+        }
+    }
+}
 
 int main(int argc, char **argv){
     pe_file *myfile = pe_file_open("./windows_exec.exe", PE_READWRITE_MODE);
@@ -19,9 +35,12 @@ int main(int argc, char **argv){
         return -1;
     }
 
+    printf("Number of sections: %d\n", number_sections);
     printf("Combinations to do: %d\n", number_combinations);
 
-
+    for(int i = 0; i <= number_sections; i++)
+        do_combination(combination, number_sections, 0, i);
+    
     // print_headers(myfile);
     // write_constant(myfile, ".text", 0xFA, 10, 12);
 
