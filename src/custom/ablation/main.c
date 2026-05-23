794c0ce84a076ae9d7ee26008c02449e4c4dea61
oriol.lopez.petit@estudiantat.upc.edu <oriol.lopez.petit@estudiantat.upc.edu>
2026-05-23 10:19:24 +0200

    Add directory copy function

--- src/custom/ablation/main.c
+++ src/custom/ablation/main.c
@@ -4,47 +4,113 @@
 #include <stdlib.h>
 #include <math.h>
 #include <stdbool.h>
+#include <dirent.h>
+#include <errno.h>
+#include <string.h>
 
-void do_combination(bool *combination, const int n, int k, int remaining){
+void do_combination(pe_file *file, bool *combination, const int n, int k, int remaining){
     if(remaining == 0){
         //act here
-
-        for(int i = 0; i < n; i++){
-            printf("%d ", combination[i]);
-        }
-        printf("\n");
+        
     } else{
         for(int i = k; i < (n - remaining + 1); i++){
             combination[i] = true;
-            do_combination(combination, n, i + 1, remaining - 1);
+            do_combination(file, combination, n, i + 1, remaining - 1);
             combination[i] = false;
         }
     }
 }
 
-int main(int argc, char **argv){
-    pe_file *myfile = pe_file_open("./windows_exec.exe", PE_READWRITE_MODE);
-    int number_sections = pe_file_number_of_sections(myfile);
-    int number_combinations = 1;
-    if(number_sections != 0)
-        number_combinations = 2 << (number_sections - 1); // do a power of 2
+int copy_binary_file(const char *source, const char *dest){
+    printf("source %s\n", source);
+    printf("destination %s\n", dest);
+    return 0;
+}
 
-    bool *combination = malloc(number_combinations * sizeof(bool));
-    if(combination == NULL){
-        pe_file_destructor(myfile);
+int copy_directory_files(const char *source, const char *dest){
+    DIR *input_dir = opendir(source);
+    if(input_dir == NULL){
+        printf("Cannot open input directory\n");
+        exit(-1);
+    }
+    
+    struct dirent *ep;
+    errno = 0;
+    while((ep = readdir(input_dir)) != NULL){
+        int source_len = strlen(source) + strlen(ep->d_name) + 1;
+        int dest_len = strlen(dest) + strlen(ep->d_name) + 1;
+        
+        char *full_source = malloc(source_len * sizeof(char));
+        if(full_source == NULL)
+            return -1;
+        char *full_dest = malloc(dest_len * sizeof(char));
+        if(full_dest == NULL){
+            free(full_source);
+            return -1;
+        }
+
+        strcpy(full_dest, dest);
+        strcpy(full_source, source);
+        strcat(full_dest, ep->d_name);
+        strcat(full_source, ep->d_name);
+
+        if(copy_binary_file(full_source, full_dest) == -1){
+            free(full_source);
+            free(full_dest);
+            return -1;
+        }
+
+        free(full_source);
+        free(full_dest);
+    }
+    if(errno != 0){
+        closedir(input_dir);
         return -1;
     }
+    return 0;    
+}
 
-    printf("Number of sections: %d\n", number_sections);
-    printf("Combinations to do: %d\n", number_combinations);
+// DIR *output_dir = opendir(dest);
+// if(output_dir == NULL){
+//     printf("Cannot open the output directory\n");
+//     exit(-1);
+// }
 
-    for(int i = 0; i <= number_sections; i++)
-        do_combination(combination, number_sections, 0, i);
+int main(int argc, char **argv){
+    if(argc < 3){
+        printf("The executable should be called as 'executable input_directory output_directory'\n");
+        exit(-1);
+    }
+
+    copy_directory_files(argv[1],argv[2]);
+
+
+    // pe_file *myfile = pe_file_open("./windows_exec.exe", PE_READWRITE_MODE);
+    // int number_sections = pe_file_number_of_sections(myfile);
+    // int number_combinations = 1;
+    // if(number_sections != 0)
+    //     number_combinations = 2 << (number_sections - 1); // do a power of 2
+
+    // printf("Number of sections: %d\n", number_sections);
+    // printf("Combinations to do: %d\n", number_combinations);
+
+    // bool *combination = malloc(number_combinations * sizeof(bool));
+    // if(combination == NULL){
+    //     pe_file_destructor(myfile);
+    //     return -1;
+    // }
+
+    // for(int i = 0; i <= number_sections; i++)
+    //     do_combination(myfile, combination, number_sections, 0, i);
     
-    // print_headers(myfile);
-    // write_constant(myfile, ".text", 0xFA, 10, 12);
+    // // print_headers(myfile);
+    // // write_constant(myfile, ".text", 0xFA, 10, 12);
 
-    free(combination);
-    pe_file_destructor(myfile);
+    // free(combination);
+    // pe_file_destructor(myfile);
+
+
+    // closedir(input_dir);
+    // closedir(output_dir);
     return 0;
 }
