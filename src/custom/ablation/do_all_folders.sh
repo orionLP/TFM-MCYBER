699b494ec079722dabcbec62225d59844f04daa7
oriol.lopez.petit@estudiantat.upc.edu <oriol.lopez.petit@estudiantat.upc.edu>
2026-05-23 22:58:49 +0200

    Finish bash script do_all_folders.sh

--- src/custom/ablation/do_all_folders.sh
+++ src/custom/ablation/do_all_folders.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+ 
 source_directories=(
     "./data/raw/exe/x86/"
     "./data/crypters/exe/x86/tutorial/"
@@ -18,6 +18,8 @@ destination_directories=(
     "./data/custom/ablation/upx/lzma/"
 )
 
+rm -rf "./data/custom/ablation/"
+
 gcc ./src/custom/ablation/main.c ./src/custom/ablation/pe_file.c -o ./src/custom/ablation/ablation
 executable="./src/custom/ablation/ablation"
 
@@ -28,6 +30,5 @@ fi
 
 for i in "${!source_directories[@]}"; do
     mkdir -p "${destination_directories[$i]}"
-    rm "${destination_directories[$i]}/*"
     "$executable" "${source_directories[$i]}" "${destination_directories[$i]}"
 done
