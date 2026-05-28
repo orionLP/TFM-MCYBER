de3a2066ef128362b21ef2e3e16566c5e7ee5b56
oriol.lopez.petit@estudiantat.upc.edu <oriol.lopez.petit@estudiantat.upc.edu>
2026-05-28 21:48:10 +0200

    First versions of custom encryption, in this case matrix multiplication

---
+++ src/crypters/multiple_information_reveal/crypter.py
@@ -0,0 +1,119 @@
+import argparse
+import lief
+import os
+
+def align(x, al):
+    """ return <x> aligned to <al> """
+    if x % al == 0:
+        return x
+    else:
+        return x - (x % al) + al
+
+def pad_data(data, al):
+    """ return <data> padded with 0 to a size aligned with <al> """
+    return data + ([0] * (align(len(data), al) - len(data)))
+
+def matrix_xor(mat1, mat2):
+    xor_result = [[0 for j in range(4)] for i in range(4)]
+    for i in range(4):
+        for j in range(4):
+            xor_result[i][j] = mat1[i][j] ^ mat2[i][j]
+    return xor_result
+
+def matrix_mul(mat1, mat2):
+    mul_result = [[0 for j in range(4)] for i in range(4)]
+    for i in range(4):
+        for j in range(4):
+            for k in range(4):
+                mul_result[i][j] += mat1[i][k] * mat2[k][j]
+            mul_result[i][j] = mul_result[i][j] % 256
+    return mul_result
+
+key_matrix = [
+    [  112,    42,   123,   202 ],
+    [  153,   128,   168,    93 ],
+    [  116,   191,     4,   160 ],
+    [  247,   192,   100,   116 ]
+]
+
+def pack_data(data):
+    result = [byte_data for byte_data in data]
+
+    number_multiplications = len(data) // 16
+    bytes_remaining = len(data) % 16
+    
+    print(f'number_multiplications {number_multiplications}')
+    print(f'bytes_remaining {bytes_remaining}')
+
+    data_matrix = None
+    last_matrix = None
+    for i in range(number_multiplications):
+        data_matrix = [[result[j] for j in range(k * 4, (k + 1) * 4)] for k in range(i * 4, (i + 1) * 4)]
+        if i != 0:
+            data_matrix = matrix_xor(data_matrix, last_matrix)
+        last_matrix = matrix_mul(data_matrix, key_matrix)
+        for k in range(i * 4, (i+1) * 4):
+            for j in range(k * 4, (k+1) * 4):
+                result[j] = last_matrix[(j % 16) // 4][(j%16) % 4]
+
+    for i in range(0, bytes_remaining // 4 + 1):
+        j_range = 4
+        if i == (bytes_remaining // 4):
+            j_range = bytes_remaining % 4
+        for j in range(j_range):
+            result[(len(data) - (len(data) % 16)) + (i * 4) + j] = result[(len(data) - (len(data) % 16)) + (i * 4) + j] ^ last_matrix[i][j] 
+
+    return result
+
+# parser = argparse.ArgumentParser(description='Pack PE binary')
+# parser.add_argument('input', metavar="FILE", help='input file')
+# parser.add_argument('-p', metavar="UNPACKER", help='unpacker .exe', required=True)
+# parser.add_argument('-o', metavar="FILE", help='output', default="packed.exe")
+
+# args = parser.parse_args()
+
+input_directory = "data/raw/exe/x86"
+output_directory = "data/crypters/exe/x86/multiple_information_reveal"
+unpacker_path = "src/crypters/multiple_information_reveal/decrypter.exe"
+
+os.makedirs(output_directory, exist_ok=True)
+
+for file in os.listdir(input_directory):
+    input_filepath = input_directory + "/" + file
+    output_filepath = output_directory + "/" + file
+
+    # open the unpack.exe binary
+    unpack_PE = lief.PE.parse(unpacker_path)
+
+    # we're going to keep the same alignment as the ones in unpack_PE,
+    # because this is the PE we are modifying
+    file_alignment = unpack_PE.optional_header.file_alignment
+    section_alignment = unpack_PE.optional_header.section_alignment
+    # read the whole file to be packed
+    with open(input_filepath, "rb") as f:
+        input_PE_data = f.read()
+
+    packed_data = list(input_PE_data) # lief expects a list, not a "bytes" object.
+    packed_data = pack_data(packed_data)
+    packed_data = pad_data(packed_data, file_alignment) # pad with 0 to align with file alignment (removes a lief warning)
+
+    packed_section = lief.PE.Section(".packed")
+    packed_section.content =  packed_data
+    packed_section.size = len(packed_data)
+    packed_section.characteristics = 0xC0000040  # MEM_READ | MEM_WRITE | CNT_INITIALIZED_DATA
+    # We don't need to specify a Relative Virtual Address here, lief will just put it at the end, that doesn't matter.
+    unpack_PE.add_section(packed_section)
+
+    # remove the SizeOfImage, which should change, as we added a section. Lief will compute this for us.
+    unpack_PE.optional_header.sizeof_image = 0
+
+
+    # save the resulting PE
+    if(os.path.exists(output_filepath)):
+        # little trick here : lief emits no warning when it cannot write because the output
+        # file is already opened. Using this function ensure we fail in this case (avoid errors).
+        os.remove(output_filepath)
+
+    builder = lief.PE.Builder(unpack_PE, lief.PE.Builder.config_t())
+    builder.build()
+    builder.write(output_filepath)
---
+++ src/crypters/multiple_information_reveal/decrypter.c
@@ -0,0 +1,298 @@
+
+/**
+Compile with :
+
+mingw32-gcc.exe unpack.c -o unpacker.exe "-Wl,--entry=__start" -nostartfiles -nostdlib -lkernel32
+
+**/
+#include <stdint.h>
+
+void *memcpy(void *dst, const void *src, unsigned int n) {
+    unsigned char *d = dst;
+    const unsigned char *s = src;
+    while (n--) *d++ = *s++;
+    return dst;
+}
+
+#include <windows.h>
+#include <winnt.h>
+
+// loads a PE in memory, returns the entry point address
+void* load_PE (char* PE_data);
+
+// some basic functions intended to limits the external libraries needed
+int mystrcmp(char* a, char* b);
+void mymemcpy(char* dst, char* src, unsigned int size);
+void mymemset(char* dst, char c, unsigned int size);
+
+#define DWORD uint32_t
+#define NUM_ROWS (4)
+#define NUM_COLUMNS (4)
+
+int key_matrix_inverse[NUM_ROWS][NUM_COLUMNS] = {
+    {124, 188, 152, 99}, 
+    {60, 88, 23, 36}, 
+    {163, 98, 190, 170},
+    {220, 89, 88, 9}
+};
+
+int data_matrix[NUM_ROWS][NUM_COLUMNS] = {0};
+int last_matrix[NUM_ROWS][NUM_COLUMNS] = {0};
+int tmp_matrix[NUM_ROWS][NUM_COLUMNS] = {0};
+
+void matrix_xor(int (*matrix_1)[NUM_COLUMNS], int (*matrix_2)[NUM_COLUMNS], int (*matrix_result)[NUM_COLUMNS]){
+    for(int i = 0; i < NUM_ROWS; i++)
+        for(int j = 0; j < NUM_COLUMNS; j++)
+            matrix_result[i][j] = matrix_1[i][j] ^ matrix_2[i][j];
+}
+
+void matrix_mul(int (*matrix_1)[NUM_COLUMNS], int (*matrix_2)[NUM_COLUMNS], int (*matrix_result)[NUM_COLUMNS]){
+    for(int i = 0; i < NUM_ROWS; i++)
+        for(int j = 0; j < NUM_COLUMNS; j++){
+            matrix_result[i][j] = 0;
+            for(int k = 0; k < NUM_ROWS; k++)
+                matrix_result[i][j] += matrix_1[i][k] * matrix_2[k][j];
+            matrix_result[i][j] %= 256;
+        }
+}
+
+void unpack_data(char* src, DWORD size) {
+    
+    int num_multiplications = size / 16;
+    int bytes_remaining = size % 16;
+    for(int i = 0; i < num_multiplications; i++){
+        for(int k = 4 * i; k < ((i + 1) * 4); k++)
+            for(int j = k * 4; j < ((k+1) * 4); j++)
+                data_matrix[k % 4][j % 4] = (unsigned char) src[j];
+        
+        matrix_mul(data_matrix, key_matrix_inverse, tmp_matrix);
+        
+        if(i != 0)
+            matrix_xor(tmp_matrix, last_matrix, tmp_matrix);
+
+        for(int j = 0; j < NUM_ROWS; j++)
+            for(int k = 0; k < NUM_COLUMNS; k++)
+                last_matrix[j][k] = data_matrix[j][k];
+
+        for(int k = i * 4; k < (i+1) * 4; k++)
+            for(int j = k * 4; j < (k+1) * 4; j++)
+                src[j] = tmp_matrix[(j % 16) / 4][(j % 16) % 4];
+    }
+    
+    for(int i = 0; i < (bytes_remaining/4) + 1; i++){
+        int j_range = 4;
+        if(i == (bytes_remaining / 4))
+            j_range = bytes_remaining % 4;
+        for(int j = 0; j < j_range; j++)
+            src[(size - (size % 16)) + (i * 4) + j] ^= last_matrix[i][j];
+    }
+}
+
+
+int _start(void) { //Entrypoint for the program
+
+    // Get the current module VA (ie PE header addr)
+    char* unpacker_VA = (char*) GetModuleHandleA(NULL);
+
+    // get to the section header
+    IMAGE_DOS_HEADER* p_DOS_HDR  = (IMAGE_DOS_HEADER*) unpacker_VA;
+    IMAGE_NT_HEADERS* p_NT_HDR = (IMAGE_NT_HEADERS*) (((char*) p_DOS_HDR) + p_DOS_HDR->e_lfanew);
+    IMAGE_SECTION_HEADER* sections = (IMAGE_SECTION_HEADER*) (p_NT_HDR + 1);
+
+    char* packed_PE = NULL;
+    char packed_section_name[] = ".packed";
+
+    // search for the ".packed" section
+    for(int i=0; i<p_NT_HDR->FileHeader.NumberOfSections; ++i) {
+        if (mystrcmp(sections[i].Name, packed_section_name)) {
+            packed_PE = unpacker_VA + sections[i].VirtualAddress;
+            unpack_data(packed_PE, sections[i].SizeOfRawData);
+            break;
+        }
+    }
+
+
+    //load the data located at the .packed section
+    if(packed_PE != NULL) {
+        void (*packed_entry_point)(void) = (void(*)()) load_PE(packed_PE);
+        packed_entry_point();
+    }
+}
+
+
+void* load_PE (char* PE_data) {
+
+    /** Parse header **/
+
+    IMAGE_DOS_HEADER* p_DOS_HDR  = (IMAGE_DOS_HEADER*) PE_data;
+    IMAGE_NT_HEADERS* p_NT_HDR = (IMAGE_NT_HEADERS*) (((char*) p_DOS_HDR) + p_DOS_HDR->e_lfanew);
+
+    DWORD hdr_image_base = p_NT_HDR->OptionalHeader.ImageBase;
+    DWORD size_of_image = p_NT_HDR->OptionalHeader.SizeOfImage;
+    DWORD entry_point_RVA = p_NT_HDR->OptionalHeader.AddressOfEntryPoint;
+    DWORD size_of_headers = p_NT_HDR->OptionalHeader.SizeOfHeaders;
+
+    /** Allocate Memory **/
+    char* ImageBase = (char*) VirtualAlloc(NULL, size_of_image, MEM_RESERVE | MEM_COMMIT, PAGE_READWRITE);
+    if(ImageBase == NULL) {
+        // Allocation failed
+        return NULL;
+    }
+
+    /** Map PE sections in memory **/
+
+    mymemcpy(ImageBase, PE_data, p_NT_HDR->OptionalHeader.SizeOfHeaders);
+
+
+    // Section headers starts right after the IMAGE_NT_HEADERS struct, so we do some pointer arithmetic-fu here.
+    IMAGE_SECTION_HEADER* sections = (IMAGE_SECTION_HEADER*) (p_NT_HDR + 1); 
+
+    // For each sections
+    for(int i=0; i<p_NT_HDR->FileHeader.NumberOfSections; ++i) {
+        // calculate the VA we need to copy the content, from the RVA 
+        // section[i].VirtualAddress is a RVA, mind it
+        char* dest = ImageBase + sections[i].VirtualAddress; 
+
+        // check if there is Raw data to copy
+        if(sections[i].SizeOfRawData > 0) {
+            // We copy SizeOfRaw data bytes, from the offset PointertoRawData in the file
+            mymemcpy(dest, PE_data + sections[i].PointerToRawData, sections[i].SizeOfRawData);
+        } else {
+            mymemset(dest, 0, sections[i].Misc.VirtualSize);
+        }
+    }
+
+    IMAGE_DATA_DIRECTORY* data_directory = p_NT_HDR->OptionalHeader.DataDirectory;
+
+    /** Handle imports **/
+    
+    // load the address of the import descriptors array
+    IMAGE_IMPORT_DESCRIPTOR* import_descriptors = (IMAGE_IMPORT_DESCRIPTOR*) (ImageBase + data_directory[IMAGE_DIRECTORY_ENTRY_IMPORT].VirtualAddress);
+
+    // this array is null terminated
+    for(int i=0; import_descriptors[i].OriginalFirstThunk != 0; ++i) {
+
+        // Get the name of the dll, and import it
+        char* module_name = ImageBase + import_descriptors[i].Name;
+        HMODULE import_module = LoadLibraryA(module_name);
+        if(import_module == NULL) {
+            return NULL;
+        }
+
+        // the lookup table points to function names or ordinals => it is the IDT
+        IMAGE_THUNK_DATA* lookup_table = (IMAGE_THUNK_DATA*) (ImageBase + import_descriptors[i].OriginalFirstThunk);
+
+        // the address table is a copy of the lookup table at first
+        // but we put the addresses of the loaded function inside => that's the IAT
+        IMAGE_THUNK_DATA* address_table = (IMAGE_THUNK_DATA*) (ImageBase + import_descriptors[i].FirstThunk);
+
+        // null terminated array, again
+        for(int i=0; lookup_table[i].u1.AddressOfData != 0; ++i) {
+            void* function_handle = NULL;
+
+            // Check the lookup table for the adresse of the function name to import
+            DWORD lookup_addr = lookup_table[i].u1.AddressOfData;
+
+            if((lookup_addr & IMAGE_ORDINAL_FLAG) == 0) { //if first bit is not 1
+                // import by name : get the IMAGE_IMPORT_BY_NAME struct
+                IMAGE_IMPORT_BY_NAME* image_import = (IMAGE_IMPORT_BY_NAME*) (ImageBase + lookup_addr);
+                // this struct points to the ASCII function name
+                char* funct_name = (char*) &(image_import->Name);
+                // get that function address from it's module and name
+                function_handle = (void*) GetProcAddress(import_module, funct_name);
+            } else {
+                // import by ordinal, directly
+                function_handle = (void*) GetProcAddress(import_module, (LPSTR) lookup_addr);
+            }
+
+            if(function_handle == NULL) {
+                return NULL;
+            }
+
+            // change the IAT, and put the function address inside.
+            address_table[i].u1.Function = (DWORD) function_handle;
+        }
+    }
+
+    /** Handle relocations **/
+
+    //this is how much we shifted the ImageBase
+    DWORD delta_VA_reloc = ((DWORD) ImageBase) - p_NT_HDR->OptionalHeader.ImageBase;
+
+    // if there is a relocation table, and we actually shitfted the ImageBase
+    if(data_directory[IMAGE_DIRECTORY_ENTRY_BASERELOC].VirtualAddress != 0 && delta_VA_reloc != 0) {
+
+        //calculate the relocation table address
+        IMAGE_BASE_RELOCATION* p_reloc = (IMAGE_BASE_RELOCATION*) (ImageBase + data_directory[IMAGE_DIRECTORY_ENTRY_BASERELOC].VirtualAddress);
+        
+        //once again, a null terminated array
+        while(p_reloc->VirtualAddress != 0) {
+
+            // how any relocation in this block
+            // ie the total size, minus the size of the "header", divided by 2 (those are words, so 2 bytes for each)
+            DWORD size = (p_reloc->SizeOfBlock - sizeof(IMAGE_BASE_RELOCATION))/2;
+            // the first relocation element in the block, right after the header (using pointer arithmetic again)
+            WORD* reloc = (WORD*) (p_reloc + 1);
+            for(int i=0; i<size; ++i) {
+                //type is the first 4 bits of the relocation word
+                int type = reloc[i] >> 12;
+                // offset is the last 12 bits
+                int offset = reloc[i] & 0x0fff;
+                //this is the address we are going to change
+                DWORD* change_addr = (DWORD*) (ImageBase + p_reloc->VirtualAddress + offset);
+
+                // there is only one type used that needs to make a change
+                switch(type){
+                    case IMAGE_REL_BASED_HIGHLOW :
+                        *change_addr += delta_VA_reloc;
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            // switch to the next relocation block, based on the size
+            p_reloc = (IMAGE_BASE_RELOCATION*) (((DWORD) p_reloc) + p_reloc->SizeOfBlock);
+        }
+    }
+
+    /** Map PE sections privileges **/
+
+    //Set permission for the PE hader to read only
+    DWORD oldProtect;
+    VirtualProtect(ImageBase, p_NT_HDR->OptionalHeader.SizeOfHeaders, PAGE_READONLY, &oldProtect);
+
+    for(int i=0; i<p_NT_HDR->FileHeader.NumberOfSections; ++i) {
+        char* dest = ImageBase + sections[i].VirtualAddress;
+        DWORD s_perm = sections[i].Characteristics;
+        DWORD v_perm = 0; //flags are not the same between virtal protect and the section header
+        if(s_perm & IMAGE_SCN_MEM_EXECUTE) {
+            v_perm = (s_perm & IMAGE_SCN_MEM_WRITE) ? PAGE_EXECUTE_READWRITE : PAGE_EXECUTE_READ;
+        } else {
+            v_perm = (s_perm & IMAGE_SCN_MEM_WRITE) ? PAGE_READWRITE : PAGE_READONLY;
+        }
+        VirtualProtect(dest, sections[i].Misc.VirtualSize, v_perm, &oldProtect);
+    }
+
+    return (void*) (ImageBase + entry_point_RVA);
+}
+
+int mystrcmp(char* a, char* b) {
+    while(*a == *b && *a) {
+        a++;
+        b++;
+    }
+    return (*a == *b);
+}
+
+void mymemcpy(char* dst, char* src, unsigned int size) {
+    for(unsigned int i=0; i<size; ++i) {
+        dst[i] = src[i];
+    }
+}
+
+void mymemset(char* dst, char c, unsigned int size) {
+    for(unsigned int i=0; i<size; ++i) {
+        dst[i] = c;
+    }   
+}
