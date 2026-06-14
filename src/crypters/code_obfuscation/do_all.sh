7eb7110c076806cca7ae7f3cbb89e33f3edb5b9f
oriol.lopez.petit@estudiantat.upc.edu <oriol.lopez.petit@estudiantat.upc.edu>
2026-06-14 19:14:01 +0200

    Script to do one round

---
+++ src/crypters/code_obfuscation/do_all.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+source_directory="./data/raw/exe/x86/"
+ 
+destination_directory="./data/crypters/exe/x86/code_obfuscation/opaque_ifs/"
+
+mkdir -p "./src/crypters/code_obfuscation/temporary_codes/"
+mkdir -p "${destination_directory}"
+
+for file in $(ls ${source_directory}); do
+    echo "Processing file ${file}"
+    cp "./src/crypters/code_obfuscation/merged.c" "./src/crypters/code_obfuscation/temporary_codes/merged.c"
+    python3 "./src/crypters/code_obfuscation/crypter.py" -o ./src/crypters/code_obfuscation/temporary_codes/merged.c "${source_directory}${file}"
+    python3 "./src/crypters/code_obfuscation/opaque_variables.py" ./src/crypters/code_obfuscation/temporary_codes/merged.c "./src/crypters/code_obfuscation/temporary_codes/transformed_merged.c"
+    clang --target=i686-pc-windows-gnu -I "./src/crypters/code_obfuscation/fake_imports" -lkernel32 -O0 "./src/crypters/code_obfuscation/temporary_codes/transformed_merged.c" -o "${destination_directory}${file}"
+done
