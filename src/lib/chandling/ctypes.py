42934b444b5e1eb90ce567d02bd70ed23256802f
oriol.lopez.petit@estudiantat.upc.edu <oriol.lopez.petit@estudiantat.upc.edu>
2026-06-28 17:31:39 +0200

    Change how ctypes are handled

--- src/lib/chandling/ctypes.py
+++ src/lib/chandling/ctypes.py
@@ -1,26 +1,18 @@
 import enum
 import dataclasses
 
-
 @dataclasses.dataclass
-class CTypeInfo:
+class CTypeInfo():
     cname: str
     min_value: int
     max_value: int
 
-class CTypei6WindowsGNU(Enum):
-# Values hardcoded for i686-pc-windows-gnu
-    UNSIGNED_INT = CTypeInfo('unsigned int', 0, 2**32 - 1)
-    INT = CTypeInfo('int', -(2**31), 2**31 - 1)
+@dataclasses.dataclass
+class CTypeTable():
+    unsigned_int: CTypeInfo
+    int: CTypeInfo
 
-    @property
-    def cname(self):   
-        return self.value.cname
-    
-    @property
-    def min_val(self): 
-        return self.value.min_val
-    
-    @property
-    def max_val(self): 
-        return self.value.max_val
+I686PCWindowsGNU: CTypeTable = CTypeTable(
+    CTypeInfo('unsigned int', 0, 2**32 - 1),
+    CTypeInfo('int', -(2**31), 2**31 - 1)
+)
