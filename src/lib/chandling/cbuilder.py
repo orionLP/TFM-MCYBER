400e32623cdda6c2346697ebc0b6f950d5e6a46b
oriol.lopez.petit@estudiantat.upc.edu <oriol.lopez.petit@estudiantat.upc.edu>
2026-06-29 12:49:19 +0200

    Add interface for constants and variable references

--- src/lib/chandling/cbuilder.py
+++ src/lib/chandling/cbuilder.py
@@ -6,8 +6,15 @@ import src.lib.chandling.coperators as coperators
 
 class CBuilder(abc.ABC):
 
-    @abc.abstractmethod
     def __init__(self, integer_definitions: ctype.CTypeTable) -> None:
+        self._integer_definitions = integer_definitions
+    
+    @abc.abstractmethod
+    def variable(self, variable_name: str) -> pycparser.c_ast.ID:
+        pass
+    
+    @abc.abstractmethod
+    def constant(self, constant_type: ctypes.CTypes, value: int) -> pycparser.c_ast.Constant:
         pass
     
     @abc.abstractmethod
@@ -33,3 +40,12 @@ class CBuilder(abc.ABC):
     @abc.abstractmethod
     def declaration(self, name: str, integer_type: ctype.CTypes, initializer: pycparser.c_ast.Node | None) -> pycparser.c_ast.Decl:
         pass
+
+class StandardCBuilder(CBuilder):
+
+    def binary_operation(self, operator: coperators.BinaryCOperator, left_operand: pycparser.c_ast.Node, right_operand: pycparser.c_ast.Node) -> pycparser.c_ast.Node:
+        return c_ast.BinaryOp(
+                    operator,
+                    left_operand,
+                    right_operand
+                )
