92336a9a6e841dd37946b3855d6c1fe937817a14
oriol.lopez.petit@estudiantat.upc.edu <oriol.lopez.petit@estudiantat.upc.edu>
2026-06-29 13:15:57 +0200

    Add block to the interface of cbuilder

--- src/lib/chandling/cbuilder.py
+++ src/lib/chandling/cbuilder.py
@@ -26,6 +26,10 @@ class CBuilder(abc.ABC):
         pass
 
     @abc.abstractmethod
+    def block(self, items_list: list[pycparser.c_ast.Node]) -> pycparser.c_ast.Compound:
+        pass
+
+    @abc.abstractmethod
     def assignment(self, operator: coperators.AssignmentCOperator, left_value: pycparser.c_ast.Node, right_value: pycparser.c_ast.Node) -> pycparser.c_ast.Assignment:
         pass
     
@@ -43,9 +47,52 @@ class CBuilder(abc.ABC):
 
 class StandardCBuilder(CBuilder):
 
+    def variable(self, variable_name: str) -> pycparser.c_ast.ID:
+        return pycparser.c_ast.ID(
+            name = variable_name
+        )
+    
+    def constant(self, constant_type: ctypes.CTypes, value: int) -> pycparser.c_ast.Constant:
+        return pycparser.c_ast.Constant(
+            type = constant_type,
+            value = value
+        )
+    
+    def unary_operation(self, operator: coperators.UnaryCOperator, operand: pycparser.c_ast.Node) -> pycparser.c_ast.Node:
+        return pycparser.c_ast.UnaryOp(
+            op = operator,
+            expr = operand
+        )
+    
     def binary_operation(self, operator: coperators.BinaryCOperator, left_operand: pycparser.c_ast.Node, right_operand: pycparser.c_ast.Node) -> pycparser.c_ast.Node:
-        return c_ast.BinaryOp(
-                    operator,
-                    left_operand,
-                    right_operand
-                )
+        return pycparser.c_ast.BinaryOp(
+            op = operator,
+            left = left_operand,
+            right = right_operand
+        )
+
+    def block(self, items_list: list[pycparser.c_ast.Node]) -> pycparser.c_ast.Compound:
+        pass
+    
+    def assignment(self, operator: coperators.AssignmentCOperator, left_value: pycparser.c_ast.Node, right_value: pycparser.c_ast.Node) -> pycparser.c_ast.Assignment:
+        return pycparser.c_ast.Assignment(
+            op = operator,
+            lvalue = left_value,
+            rvalue = right_value
+        )
+    
+    def if_block(self, condition: pycparser.c_ast.Node, true_block: pycparser.c_ast.Compound, false_block: pycparser.c_ast.Compound) -> pycparser.c_ast.If:
+        return pycparser.c_ast.If(
+            cond = condition,
+            iftrue = true_block,
+            iffalse = false_block
+        )
+    
+    def while_block(self, condition: pycparser.c_ast.Node, statement_block: pycparser.c_ast.Compound) -> pycparser.c_ast.While:
+        return pycparser.c_ast.While(
+            cond = condition,
+            stmt = statement_block
+        )
+    
+    def declaration(self, name: str, integer_type: ctype.CTypes, initializer: pycparser.c_ast.Node | None) -> pycparser.c_ast.Decl:
+        pass
