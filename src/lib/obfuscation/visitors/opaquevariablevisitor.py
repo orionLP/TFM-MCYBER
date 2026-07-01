321e4d256cf3a479b99b180776cf4d675475cf5f
oriol.lopez.petit@estudiantat.upc.edu <oriol.lopez.petit@estudiantat.upc.edu>
2026-07-01 18:50:28 +0200

    Start visitor to add variables

---
+++ src/lib/obfuscation/visitors/opaquevariablevisitor.py
@@ -0,0 +1,52 @@
+import abc
+import pycparser
+
+from src.lib.crypto.rng import prng
+
+import src.lib.chandling.ctypes as ctypes
+import src.lib.chandling.cbuilder as cbuilder
+import src.lib.chandling.coperators as coperators
+import src.lib.obfuscation.utils.namegenerator as namegenerator
+import src.lib.obfuscation.utils.scope as scope
+import src.lib.obfuscation.control.opaquepredicate as opaquepredicate
+import src.lib.obfuscation.control.opaquevariable as opaquevariable
+
+class InjectOpaqueVariableVisitor(pycparser.c_ast.NodeVisitor):
+
+    def __init__(
+        self,
+        used_cbuilder: cbuilder.CBuilder,
+        used_opaque_variables: opaquevariable.OpaqueVariable,
+        target_variable_type: ctypes.CTypes,
+        upper_block_scope: scope.Scope,
+        max_depth: int = 16, 
+        aggressiveness: float = 0.3
+    ) -> None:
+
+        self._cbuider = used_cbuilder
+        self._opaque_variable = used_opaque_variables
+        self._upper_block_scope = upper_block_scope
+        self._target_type = target_variable_type
+        self._max_depth = max_depth
+        self._aggressiveness = aggressiveness
+
+    def visit_Compound(self, node):
+        self._upper_block_scope.enter_block()
+
+        if node.block_items is None:
+            node.block_items = []
+
+        if self._upper_block_scope == 1:
+            new_block = self._cbuider.block(node.block_items)
+            node.block_items = [new_block]
+
+        if prng.chance(self._aggressiveness):
+            position_to_place = prng.get_range_unsigned_integer(len(node.block_items) + 1)
+            node.block_items.insert
+            (
+                position_to_place, 
+                self._opaque_variable.generate_opaque_variable(self._target_type)
+            )
+        self.generic_visit(node)
+
+        self._upper_block_scope.enter_block()
