9e5228c79098cf5941deb4a1ca320fee19304e23
oriol.lopez.petit@estudiantat.upc.edu <oriol.lopez.petit@estudiantat.upc.edu>
2026-07-05 22:52:09 +0200

    Work

--- src/crypters/idata_obfuscation/extract_headers_v2.py
+++ src/crypters/idata_obfuscation/extract_headers_v2.py
@@ -1,90 +1,6 @@
-from clang.cindex import Index, CursorKind, TypeKind
-from collections import defaultdict
-import abc
-
-class HeaderExtractor(abc.ABC):
-
-    @abc.abstractmethod
-    def parse(self, header_path: str) -> None:
-        pass
-
-    @abc.abstractmethod
-    def list_functions(self):
-        pass
-
-class StandardHeaderExtractor(HeaderExtractor):
-
-    def __init__(self) -> None:
-        self._processed_headers = None
-        self._tu = None
-        self._index = Index.create()
-
-    def parse(self, header_path: str) -> None:
-        tu = self._index.parse(header_path)
-        self._tu = tu
-        self._processed_headers = header_path
-
-    def list_functions(self):
-        return_list = []
-        main_file = self._tu.cursor.extent.start.file.name
-        for cursor in self._tu.cursor.get_children():
-            function_location = cursor.location.file.name
-            if cursor.kind == CursorKind.FUNCTION_DECL and main_file == function_location:
-                return_list.append(cursor.spelling)
-        return return_list
-
-import subprocess
-import re
-
-class ArchiveHandler(abc.ABC):
-
-    @abc.abstractmethod
-    def list_functions(self, file_path: str) -> list[str]:
-        pass
-    
-class ProcessArchiveHandler(ArchiveHandler):
-
-    def list_functions(self, file_path: str) -> list[str]:
-        result = subprocess.run(
-            ['i686-w64-mingw32-nm', file_path],
-            capture_output=True,
-            text=True,
-            check=False
-        )
-
-        return_list = []
-        for line in result.stdout.split('\n'):
-            found = not re.match("[0-9]* I __imp__[A-Z]", line) is None
-            if found:
-                name = re.search("__imp__.*@",line)
-                if not name is None:
-                    return_list.append(name.group(0)[7:-1])
-
-        return return_list            
-
-
-class EligibleFunctions(abc.ABC):
-
-    @abc.abstractmethod
-    def parse(self, header_path: str) -> None:
-        pass
-
-    @abc.abstractmethod
-    def eligible_functions(self, function_list: list[str], header_file_path: str) -> list[str]:
-        pass
-
-class StandardEligibleFunctions(EligibleFunctions):
-
-    def parse(self, header_path: str) -> None:
-        self._processed_headers = None
-        self._tu = None
-        self._reverse_nodes = []
-        self._index = Index.create()
-    
-    def eligible_functions(self, function_list: list[str], header_file_path: str) -> list[str]:
-        pass
-
 import csv
+
+import src.lib.chandling.functionextractors as fe
 
 if __name__ == "__main__":
     relation_header_libraries = {}
@@ -100,18 +16,16 @@ if __name__ == "__main__":
             archive_file = row[1]
             relation_header_libraries[header_file] = archive_file
 
-    extractor = StandardHeaderExtractor()
-    pah = ProcessArchiveHandler()
+    extractor = fe.HFileHeaderFunctionExtractor()
+    pah = fe.ArchiveHeaderFunctionExtractor()
     for header, library in relation_header_libraries.items():
 
         path_to_include = '/usr/i686-w64-mingw32/include/' + header
         path_to_library = '/usr/i686-w64-mingw32/lib/' + library
         
-        extractor.parse(path_to_include)
-        functions_in_header = set(extractor.list_functions())
+        functions_in_header = set(extractor.list_functions(path_to_include))
         
         functions_in_archive = set(pah.list_functions(path_to_library))
 
         print(f'Header: {header}, Library {library}')
         print(f'Number of functions {len(functions_in_header.intersection(functions_in_archive))}')
-        # print(functions_in_header.intersection(functions_in_archive))
--- src/crypters/idata_obfuscation/header_resolver.py
+++ src/crypters/idata_obfuscation/header_resolver.py
@@ -4,135 +4,11 @@ from clang.cindex import Index, CursorKind, TypeKind
 import sys
 from collections import defaultdict
 
+
 from clang.cindex import TypeKind
 
-BUILTIN_KINDS = {
-    TypeKind.VOID, TypeKind.BOOL, TypeKind.CHAR_U, TypeKind.UCHAR, TypeKind.CHAR16, TypeKind.CHAR32,
-    TypeKind.USHORT, TypeKind.UINT, TypeKind.ULONG, TypeKind.ULONGLONG, TypeKind.UINT128,
-    TypeKind.CHAR_S, TypeKind.SCHAR, TypeKind.WCHAR, TypeKind.SHORT, TypeKind.INT, TypeKind.LONG, 
-    TypeKind.LONGLONG, TypeKind.INT128, TypeKind.FLOAT, TypeKind.DOUBLE, TypeKind.LONGDOUBLE,
-    TypeKind.NULLPTR,
-}
+import networkx as nx
 
-def is_primitive(cursor: clang.cindex.Cursor) -> bool:
-    return cursor in BUILTIN_KINDS
-
-def is_function_declaration(cursor: clang.cindex.Cursor) -> bool:
-    return cursor.kind == CursorKind.FUNCTION_DECL and not cursor.is_definition()
-
-import networkx as nx 
-
-class HeaderResolver:
-    def __init__(self) -> None:
-        self._tu = None
-        self._file_path = None
-        self._function_nodes = {}
-        self._index = Index.create()
-        
-    def parse(self, file_path: str, include_dirs: list[str]) -> None:
-        args = [f"-I{directory}" for directory in include_dirs]
-        self._tu = self._index.parse(file_path, args=args)
-
-        for cursor in self._tu.cursor.get_children():
-            if is_function_declaration(cursor):
-                self._function_nodes[cursor.spelling] = cursor
-    
-    def _unwrap(self, t: clang.cindex.Type) -> list[clang.cindex.Type]:
-        while True:
-            if t.kind == TypeKind.POINTER:
-                t = t.get_pointee()
-            elif t.kind in (TypeKind.CONSTANTARRAY, TypeKind.INCOMPLETEARRAY):
-                t = t.get_array_element_type()
-            elif t.kind == TypeKind.ELABORATED:
-                t = t.get_named_type()
-            else:
-                return t
-    
-    def _clean_types(self, type_list: list[clang.cindex.Type]) -> list[clang.cindex.Type]:
-        clean_list = []
-
-        for item in type_list:
-            unwrapped = self._unwrap(item)
-            if is_primitive(unwrapped.kind):
-                continue
-            elif unwrapped.kind in (TypeKind.FUNCTIONPROTO, TypeKind.FUNCTIONNOPROTO):
-                if unwrapped.kind == TypeKind.FUNCTIONPROTO:
-                    for argument in unwrapped.argument_types():
-                        clean_list += self._clean_types([argument])
-                clean_list += self._clean_types([unwrapped.get_result()])
-            else:
-                clean_list += [unwrapped]
-        
-        return clean_list
-
-    def _construct_dependencies(self, dependency_graph: nx.DiGraph, start_node: str) -> None:
-        cursor = dependency_graph.nodes[start_node]['cursor']
-        needed_types = []
-        if is_function_declaration(cursor): 
-            needed_types += self._clean_types(list(cursor.type.argument_types()) + [cursor.result_type])
-        if cursor.kind == CursorKind.TYPEDEF_DECL:
-            needed_types += self._clean_types([cursor.underlying_typedef_type])
-        if cursor.kind in (CursorKind.STRUCT_DECL, CursorKind.UNION_DECL):
-            for field in cursor.get_children():
-                if field.kind == CursorKind.FIELD_DECL:
-                    needed_types += self._clean_types([field.type])
-
-        types_declarations = [item.get_declaration() for item in needed_types]
-        for item in needed_types:
-            cursor = item.get_declaration()
-            identifier = cursor.get_usr()
-            was_before = identifier in dependency_graph.nodes
-            if not was_before:
-                dependency_graph.add_node(identifier, cursor = cursor)
-            dependency_graph.add_edge(start_node, identifier)
-            if not was_before:
-                self._construct_dependencies(dependency_graph, identifier)
-
-
-    def _construct_dependency_graph(self, function_node: clang.cindex.Cursor, previous_graph: nx.DiGraph | None = None) -> nx.DiGraph:
-        dependency_graph = None
-        if previous_graph is None:
-            dependency_graph = nx.DiGraph()
-        else:
-            dependency_graph = previous_graph
-        dependency_graph.add_node(function_node.get_usr(), cursor = function_node)
-        self._construct_dependencies(dependency_graph, function_node.get_usr())
-        return dependency_graph
-    
-    def resolve_dependencies(self, func_name: str, previous_graph: nx.DiGraph | None = None) -> list[clang.cindex.Cursor]:
-        '''Returns a list of all dependencies and the function node'''
-        function_node = self._function_nodes[func_name]
-        dependency_graph = self._construct_dependency_graph(function_node, previous_graph)
-        return dependency_graph
-
-    def sort_dependencies(self, graph: nx.DiGraph) -> list[str]:
-        return list(reversed(list(nx.topological_sort(graph))))
-
-    def get_source_text(self, cursor) -> str:
-        extent = cursor.extent
-        start = extent.start.offset
-
-        filename = extent.start.file.name
-        with open(filename, 'rb') as f:
-            content = f.read()
-
-        # for some reason for typedefs libclang refuses to work
-        if cursor.kind == CursorKind.TYPEDEF_DECL:
-            end = content.find(b';', start)
-        else:
-            end = extent.end.offset
-        
-        return content[start:end].decode('utf-8')
-        
-    def print_code(self, graph: nx.DiGraph, topological_sort: list[str]) -> str:
-        final_string = ""
-
-        for resource in topological_sort:
-            node = graph.nodes[resource]['cursor']
-            final_string += self.get_source_text(node)
-            final_string += ";\n"
-
-        return final_string
 
 import matplotlib.pyplot as plt
 
@@ -141,18 +17,24 @@ import matplotlib.pyplot as plt
 # command used:
 # clang  --target=i686-pc-windows-gnu -E -I/usr/i686-w64-mingw32/include /usr/i686-w64-mingw32/include/windows.h -o preprocessed.i
 
+import src.lib.chandling.dependencyresolver as dependencyresolver
+import src.lib.chandling.headerhandler as headerhandler
+
 if __name__ == "__main__":
-    extractor = HeaderResolver()
-    extractor.parse('./preprocessed.i', '/usr/i686-w64-mingw32/include/')
+    extractor = dependencyresolver.ClangDependencyResolver()
+    extractor.parse('./data/preprocessed_headers/winbase.h', '/usr/i686-w64-mingw32/include/')
 
-    graph = extractor.resolve_dependencies('VirtualAlloc')
-    graph = extractor.resolve_dependencies('VirtualProtect', graph)
-    graph = extractor.resolve_dependencies('GetProcAddress', graph)
 
-    dependencies_listed = extractor.sort_dependencies(graph)
-    generated_code = extractor.print_code(graph, dependencies_listed)
-    print(generated_code)
+    # graph = extractor.resolve_dependencies('BeginUpdateResourceA')
+    graph = extractor.resolve_dependencies('AccessCheckAndAuditAlarmA')
+    # graph = extractor.resolve_dependencies('GetProcAddress', graph)
 
+    handler = headerhandler.StandardHeaderResolver()
+    clang_text, pycparser_text = handler.print_code(graph)
+
+    # print(clang_text)
+    print(pycparser_text)
+  
     # Create layout with better spacing
     pos = nx.spring_layout(graph, seed=42, k=2, iterations=50)
 
