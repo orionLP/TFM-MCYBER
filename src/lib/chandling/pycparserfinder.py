a8fce3767d8407697fbf2f62a4ccbcb827f45711
oriol.lopez.petit@estudiantat.upc.edu <oriol.lopez.petit@estudiantat.upc.edu>
2026-07-07 23:19:18 +0200

    Delete print

--- src/lib/chandling/pycparserfinder.py
+++ src/lib/chandling/pycparserfinder.py
@@ -12,7 +12,6 @@ class ItemFinder(abc.ABC, pycparser.c_ast.NodeVisitor):
 
     def visit_body(self, node):
         if self._found_condition(node):
-            print(node)
             self._found_item = node
         if self._found_item is None:
             self.generic_visit(node)
