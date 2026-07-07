f7a4f0a8ebd68bd39476a99bad2ef683fd60f46b
oriol.lopez.petit@estudiantat.upc.edu <oriol.lopez.petit@estudiantat.upc.edu>
2026-07-07 17:40:26 +0200

    Increase functions to determine type

--- src/lib/chandling/pycparsertypes.py
+++ src/lib/chandling/pycparsertypes.py
@@ -2,3 +2,9 @@ import pycparser
 
 def is_function_declaration(node: pycparser.c_ast.Node) -> bool:
     return isinstance(node.type, pycparser.c_ast.FuncDecl) and isinstance(node, pycparser.c_ast.Decl)
+
+def is_struct(node: pycparser.c_ast.Node) -> bool:
+    return isinstance(node.type, pycparser.c_ast.Struct)
+
+def is_enum(node: pycparser.c_ast.Node) -> bool:
+    return isinstance(node.type, pycparser.c_ast.Enum)
