b56c7b3a60d778276d6efe4f6b6ec539554ca0a3
oriol.lopez.petit@estudiantat.upc.edu <oriol.lopez.petit@estudiantat.upc.edu>
2026-07-07 17:40:01 +0200

    Add finders for structs and enums

--- src/lib/chandling/pycparserfinder.py
+++ src/lib/chandling/pycparserfinder.py
@@ -10,17 +10,33 @@ class ItemFinder(abc.ABC, pycparser.c_ast.NodeVisitor):
         self.visit(ast)
         return self._found_item
 
-    @abc.abstractmethod
-    def _found_condition(self, node: pycparser.c_ast.Node) -> bool:
-        pass 
-
-class FunctionDeclarationFinder(ItemFinder):
-
-    def _found_condition(self, node: pycparser.c_ast.Node) -> bool:
-        return pycparsertypes.is_function_declaration(node) and node.name == self._item_to_search
-    
-    def visit_Decl(self, node):
+    def visit_body(self, node):
         if self._found_condition(node):
             self._found_item = node
         if self._found_item is None:
             self.generic_visit(node)
+
+    @abc.abstractmethod
+    def _found_condition(self, node: pycparser.c_ast.Node) -> bool:
+        pass 
+
+class DeclarationFinder(ItemFinder):
+
+    def visit_Decl(self, node):
+        self.visit_body(node)
+
+class FunctionDeclarationFinder(DeclarationFinder):
+
+    def _found_condition(self, node: pycparser.c_ast.Node) -> bool:
+        return pycparsertypes.is_function_declaration(node) and node.name == self._item_to_search
+    
+class StructFinder(DeclarationFinder):
+
+    def _found_condition(self, node: pycparser.c_ast.Node) -> bool:
+        return pycparsertypes.is_struct(node) and node.name == self._item_to_search
+
+class EnumFinder(DeclarationFinder):
+
+    def _found_condition(self, node: pycparser.c_ast.Node) -> bool:
+        return pycparsertypes.is_enum(node) and node.name == self._item_to_search
+
